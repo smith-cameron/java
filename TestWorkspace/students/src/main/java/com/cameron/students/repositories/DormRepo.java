f84f2ac7ee7158ec83adb98a5ec4570343070d6e
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-02-15 02:01:00 -0800

    update

--- TestWorkspace/students/src/main/java/com/cameron/students/repositories/DormRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cameron.students.repositories;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import com.cameron.students.models.Dorm;
-
-public interface DormRepo extends CrudRepository<Dorm, Long>{
-	List<Dorm> findAll();
-}
